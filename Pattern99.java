948f80f01dbf51e22b722180f481ede31af6924a
SreeharshaKaranam <sreeharshakaranam1989@gmail.com>
2017-07-18 09:43:52 +0530

    Add files via upload

---
+++ Pattern99.java
@@ -0,0 +1,46 @@
+package com.wordpress.sreeharshasite;
+
+public class Pattern99 {
+	
+	public static void main(String[] args)
+	 
+	{
+	 
+	int counter=0;
+	 
+	for(int i=1;i<=7;i++)
+	 
+	{
+	 
+	counter=i;
+	 
+	for(int j=1;j<=7;j++)
+	 
+	{
+	 
+	if(i>j)
+	 
+	{
+	 
+	System.out.print(" ");
+	 
+	}
+	 
+	else
+	 
+	{
+	 
+	System.out.print(counter+" ");
+	 
+	counter++;
+	 
+	}
+	 
+	}
+	 
+	System.out.println();
+	 
+	}
+	}
+	}
+
